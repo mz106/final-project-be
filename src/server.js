c5836b605b89ab04953720e7fa48fdef2948c5dc
Michael Zwierzanski <69387856+mz106@users.noreply.g>
2024-10-10 14:28:04 +0100

    edit health route

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const serverless = require("serverless-http");
 
-const connection = require("./db/connection");
 const userRouter = require("./users/routes");
 
 const port = process.env.PORT || 5001;
@@ -14,16 +12,10 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get("/health", (req, res) => {
+app.get("/.netlify/functions/health", (req, res) => {
   res.status(200).json({ message: "API is healthy" });
 });
 
 app.use(userRouter);
 
-// app.listen(port, () => {
-//   connection();
-//   console.log(`server is listening on port ${port}`);
-// });
-
 module.exports = app;
-// module.exports.handler = serverless(app);
