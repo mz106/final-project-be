ed7334812b0f937e859f1264538d3379a3e08e59
Michael Hampson <mdavidhampson@gmail.com>
2024-10-10 11:19:06 +0100

    chore: set up mongodb

--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
+const connection = require("./db/connection");
+const userRouter = require("./users/routes");
 
 const port = process.env.PORT || 5001;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
+
+connection();
+app.use(userRouter);
 
 app.get("/health", (req, res) => {
     res.status(200).json({message: "API is healthy"});
---
+++ src/users/controllers.js
@@ -0,0 +1,20 @@
+const User = require("./model");
+
+const signup = async (req, res) => {
+    try {
+        const user = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+          });
+        
+      res.status(201).json({message: "success", user: user})
+    } catch (error) {
+        res.status(501).json({message: error.message, error: error})
+    }
+  }
+
+  module.exports = {
+    signup: signup,
+
+  };
